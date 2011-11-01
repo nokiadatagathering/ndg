d485d9a61eec8dd6a3b43df83f4f7f0edd498519
Rodrigo Ramos <bstritium@gmail.com>
2011-11-01 16:27:45 -0400

    Updating ndg-server-servlets license headers
    Signed-off-by: Rodrigo Ramos <bstritium@gmail.com>

--- ndg-server-servlets/src/main/java/br/org/indt/ndg/servlets/LanguageList.java
+++ ndg-server-servlets/src/main/java/br/org/indt/ndg/servlets/LanguageList.java
@@ -1,4 +1,23 @@
 /*
+*  Nokia Data Gathering
+*
+*  Copyright (C) 2011 Nokia Corporation
+*
+*  This program is free software; you can redistribute it and/or
+*  modify it under the terms of the GNU Lesser General Public
+*  License as published by the Free Software Foundation; either
+*  version 2.1 of the License, or (at your option) any later version.
+*
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+*  Lesser General Public License for more details.
+*
+*  You should have received a copy of the GNU Lesser General Public License
+*  along with this program.  If not, see <http://www.gnu.org/licenses/
+*/
+
+/*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
